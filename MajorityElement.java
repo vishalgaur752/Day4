9ce5a816f0952fe897696b19f582ee41f1b51e1a
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-17 13:28:23 +0530

    InstaDSA

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,3 +1,37 @@
+import java.util.*;
+
 public class MajorityElement {
-    
+    public static int majorityElem(int nums[]) {
+        int majorityElement = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > nums.length / 2) {
+                majorityElement = key;
+            }
+        }
+        return majorityElement;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 2, 2, 1, 1, 1, 2, 2 };
+        System.out.println(majorityElem(nums));
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        // // if (map.containsKey(nums[i])) {
+        // // map.put(nums[i], map.get(nums[i])+1);
+        // // } else {
+        // // map.put(nums[i], 1);
+        // // }
+        // map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // }
+        // for (Integer key : map.keySet()) {
+        // if (map.get(key) > nums.length / 2) {
+        // System.out.println(key);
+        // }
+        // }
+    }
 }
--- MaximumSubArray.java
+++ MaximumSubArray.java
@@ -1,3 +1,27 @@
 public class MaximumSubArray {
-    
+    public static void kadanes(int arr[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { -1, -2, -4, -1, -2, -1, -5, -3 };
+        kadanes(arr);
+    }
 }
